5b683c576763d3b74a4720c4a307df317b74ef45
ANDRIAN Rajotiana <andrian.rajotiana@gmail.com>
2022-12-28 23:00:32 +0300

    commit

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,29 +1,15 @@
 import React from "react";
 import "./Home.css";
 import Section1 from "./Section1";
-
-function Section2() {
-  return <div className="home-section home-section-2">Section 2</div>;
-}
-
-function Section3() {
-  return (
-    <div className="home-section home-section-3">
-      <Footer />
-    </div>
-  );
-}
-
-function Footer() {
-  return <div>Footer</div>;
-}
+import Section2 from "./Section2";
+import Section3 from "./Section3";
 
 function Home() {
   return (
     <div>
       <Section1 />
-      {/* <Section2 />
-      <Section3 /> */}
+      <Section2 />
+      <Section3 />
     </div>
   );
 }
